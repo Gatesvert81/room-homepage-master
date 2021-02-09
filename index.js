aa584e6b3c986807c04ba4cad7ea01c28f398eff
Gatesvert81 <63714591+Gatesvert81@users.noreply.github.com>
2021-02-09 19:17:50 +0000

    yh

---
+++ index.js
@@ -0,0 +1,44 @@
+let handburger = document.getElementsByClassName("btn-handburger")
+
+let navShow = document.getElementsByClassName("nav-show")
+
+let hideNav = document.getElementsByClassName("hide-nav")
+
+function showNav() {
+  navShow[0].style.display = "none";
+  hideNav[0].style.display = "inline-flex";
+}
+
+function navHide() {
+  navShow[0].style.display = "flex";
+  hideNav[0].style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
